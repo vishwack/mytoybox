4532d42e4f94db282a4244fbb6affc76f1e8940d
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-04-01 12:11:45 -0700

    Cleaning up parser, updating all samples

---
+++ ludown/index.js
@@ -0,0 +1,17 @@
+const program = require('commander');
+const fParser = require('./parser');
+program
+    .arguments('<file>')
+    .option('-s, --luis_schema_version <luis_schema_version>', 'LUIS Schema version')
+    .option('-d, --luis_versionId <versionId>', 'LUIS app version')
+    .option('-n, --luis_name <lName>', 'LUIS app name')
+    .option('-d, --luis_desc <desc>', 'LUIS app description')
+    .option('-c, --luis_culture <culture>', 'LUIS app culture')
+    .option('-m, --qna_name <qName>', 'QnA KB name')
+    //.option('-l, --luis-output-file <lOutFile>', 'LUIS output file name Default is <inputfilename>_LUISApp.json')
+    //.option('-a, --qna-output-file <qOutFile>', 'QnA maker output file name. Default is <inputfilename>_qnaKB.json')
+    .option('-q, --quiet', 'Quiet, no trace messages')
+    .action(function(rootFile) {
+        fParser.handleFile(rootFile, program);
+    })
+    .parse(process.argv);
--- ludown/parser.js
+++ ludown/parser.js
@@ -223,146 +223,6 @@ var mergeResults = function(blob, finalCollection, type) {
         });
     }
 }
-var validateAndPushCurrentBuffer = function(previousSection, sectionsInFile, currentSectionType, lineIndex) {
-    switch(currentSectionType) {
-        case PARSERCONSTS.INTENT:
-            // warn if there isnt at least one utterance in an intent
-            if(previousSection.split(/\r\n/).length === 1)  {
-                console.warn(lineIndex + ': [WARN] No utterances found for intent: ' + previousSection.split(/\r\n/)[0]);
-            }
-            sectionsInFile.push(previousSection);
-            break;
-        case PARSERCONSTS.QNA:
-            // warn if there isnt at least one utterance in an intent
-            if(previousSection.split(/\r\n/).length === 1)  {
-                console.warn(lineIndex + ': [WARN] No answer found for question' + previousSection.split(/\r\n/)[0]);
-            }
-            sectionsInFile.push(previousSection);
-            break;
-        case PARSERCONSTS.ENTITY:
-            // warn if there isnt at least one utterance in an intent
-            if(previousSection.split(/\r\n/).length === 1)  {
-                console.warn(lineIndex + ': [WARN] No list entity definition found for entity:' + previousSection.split(/\r\n/)[0]);
-            }
-            sectionsInFile.push(previousSection);
-            break;
-        default:
-            sectionsInFile.push(previousSection);
-    }
-    return sectionsInFile;
-}
-var splitFileBySections = function(fileContent) {
-    var linesInFile = fileContent.split(/\n|\r\n/);
-    var currentSection = null;
-    var middleOfSection = false;
-    var sectionsInFile = [];
-    var currentSectionType = null; //PARSERCONSTS
-    for(lineIndex in linesInFile) {
-        var currentLine = linesInFile[lineIndex].trim();
-        // skip line if it is just a comment
-        if(currentLine.indexOf(PARSERCONSTS.COMMENT) === 0) continue;
-        // skip line if it is blank
-        if(currentLine === "") continue;
-        // drop any contents in this line after a comment block
-        // e.g. #Greeting //this is the root intent should return #Greeting
-        // #Greeting \// this should be considered should return everything
-        var currentLineWithoutComments = currentLine.split(/\/\/\s*/); 
-        if(currentLineWithoutComments.length > 0) {
-            // exclude http[s]://
-            if(!currentLineWithoutComments[0].includes('http')) currentLine = currentLineWithoutComments[0].trim();
-        }
-
-        // is this a FILEREF or URLREF section? 
-        if((currentLine.indexOf(PARSERCONSTS.FILEREF) === 0) ||
-           (currentLine.indexOf(PARSERCONSTS.URLREF) === 0)) {
-            // TODO: handle anything in currentSection buffer
-            if(currentSection !== null) {
-                var previousSection = currentSection.substring(0, currentSection.lastIndexOf("\r\n"));
-                sectionsInFile = validateAndPushCurrentBuffer(previousSection, sectionsInFile, currentSectionType, lineIndex);
-            }
-            currentSection = null;
-            sectionsInFile.push(currentLine);
-            middleOfSection = false;
-        } else if((currentLine.indexOf(PARSERCONSTS.INTENT) === 0)) {
-            // TODO: handle anything in currentSection buffer
-            if(currentSection !== null) {
-                var previousSection = currentSection.substring(0, currentSection.lastIndexOf("\r\n"));
-                sectionsInFile = validateAndPushCurrentBuffer(previousSection, sectionsInFile, currentSectionType, lineIndex);
-            }
-            middleOfSection = true;
-            currentSectionType = PARSERCONSTS.INTENT;
-            currentSection = currentLine + "\r\n";
-        } else if((currentLine.indexOf(PARSERCONSTS.ENTITY) === 0)) {
-            // TODO: handle anything in currentSection buffer
-            if(currentSection !== null) {
-                var previousSection = currentSection.substring(0, currentSection.lastIndexOf("\r\n"));
-                sectionsInFile = validateAndPushCurrentBuffer(previousSection, sectionsInFile, currentSectionType, lineIndex);
-            }
-            // only list entity types can have multi-line definition
-            if(currentLine.includes(':list')){
-                middleOfSection = true;
-                currentSectionType = PARSERCONSTS.ENTITY;
-                currentSection = currentLine + "\r\n";
-            } else {
-                sectionsInFile.push(currentLine);
-                middleOfSection = false;
-                currentSection = null;
-            }
-        } else if((currentLine.indexOf(PARSERCONSTS.QNA) === 0)) {
-            // there can be multiple questions to answer here. So keep adding.
-            middleOfSection = true;
-            currentSectionType = PARSERCONSTS.QNA;
-            if(currentSection !== null) {
-                currentSection += currentLine + "\r\n";
-            } else {
-                currentSection = currentLine + "\r\n";
-            }            
-        } else {
-            if(middleOfSection) {
-                currentSection += currentLine + "\r\n";
-            } else {
-                console.log('Error: Line ' + lineIndex + ' is not part of a Intent/ Entity/ QnA');
-                console.log('Stopping further processing.');
-                process.exit(1);
-            }
-        }
-/*
-        // see if current line has a parser identifier
-        if((currentLine.indexOf(PARSERCONSTS.INTENT) === 0)  ||
-           (currentLine.indexOf(PARSERCONSTS.ENTITY) === 0)  ||
-           (currentLine.indexOf(PARSERCONSTS.QNA) === 0)     ||
-           (currentLine.indexOf(PARSERCONSTS.FILEREF) === 0) || 
-           (currentLine.indexOf(PARSERCONSTS.URLREF) === 0)) {
-                // this is a new section. Push currentSection to sectionsInFile.
-                if(currentSection !== null) {
-                    // drop the last /r/n and push
-                    sectionsInFile.push(currentSection.substring(0, currentSection.lastIndexOf("\r\n")));
-                }
-                currentSection = currentLine + "\r\n";
-                middleOfSection = true;
-           } else {
-               if(middleOfSection) {
-                   currentSection += currentLine + "\r\n";
-               } else {
-                   console.log('Error: Line ' + lineIndex + ' is not part of a Intent/ Entity/ QnA');
-                   console.log('Stopping further processing.');
-                   process.exit(1);
-               }
-           }
-    }
-    // drop the last /r/n and push
-    sectionsInFile.push(currentSection.substring(0, currentSection.lastIndexOf("\r\n")));
-*/
-    }
-    // TODO: handle anything in currentSection buffer
-    if(currentSection !== null) {
-        var previousSection = currentSection.substring(0, currentSection.lastIndexOf("\r\n"));
-        sectionsInFile = validateAndPushCurrentBuffer(previousSection, sectionsInFile, currentSectionType, lineIndex);
-    }
-    console.log('FINAL SECTIONS IN FILE');
-    console.log(JSON.stringify(sectionsInFile, null, 2));
-    return sectionsInFile;
-}
 var parseFile = function(fileContent, log) 
 {
     var additionalFilesToParse = new Array();
@@ -398,8 +258,8 @@ var parseFile = function(fileContent, log)
         "qnaPairs": new Array(),
         "urls": new Array()
     }
-    var splitOnBlankLines = splitFileBySections(fileContent.toString());
-
+    // split on blank lines
+    var splitOnBlankLines = fileContent.toString().split(/\n\s*\n|\r\n\s*\r\n/);
     // loop through every chunk of information
     splitOnBlankLines.forEach(function(chunk) {
         // is this an intent or entity?
@@ -691,8 +551,7 @@ const PARSERCONSTS = {
     QNA: "?",
     URLREF: "#url",
     COMMENT: "//"
-};
-
+}
 
 var addItemIfNotPresent = function(collection, type, value) {
     var hasValue = false;
@@ -710,4 +569,4 @@ var addItemIfNotPresent = function(collection, type, value) {
         }
         collection[type].push(itemObj);
     }  
-};
+}
